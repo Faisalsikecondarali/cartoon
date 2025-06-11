af517ea49774dd93d6033de76c8d2b9afc8d7a2d
Faisalsikecondarali <muhammadfaisal013612@gmail.com>
2025-06-11 14:10:59 +0500

    Ready for Railway

--- sastaflix/settings.py
+++ sastaflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6&#i7-1ufy-5p#q%4gs#e8b@o2u!v#aab3_y=bh38tu5dn^ll1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WithNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'core/static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
